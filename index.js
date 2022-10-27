e31ab8419dfab179f0183c1a76220d5b1d97913e
James Pace <pacej2022@gmail.com>
2022-10-27 15:34:29 -0700

    update code

--- index.js
+++ index.js
@@ -1,1 +1,92 @@
 //Add server code here!
+const express = require("express")
+const app = express();
+const path = require("path")
+const fs = require("fs")
+const PORT = 3000;
+
+app.use(express.static("public"))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname,"./views/index.html"))
+})
+
+app.get('/api/books',(req, res) => {
+    fs.readFile("./db/books.json", "utf-8", (err,data)=>{
+        if(err) {
+            console.log(err)
+            res.status(500).json({
+                msg:"oh no!",
+                err:err
+            })
+        } else {
+            const dataArr = JSON.parse(data)
+            return res.json(dataArr)
+        }
+    })
+
+})
+
+app.get('/api/books/:id',(req, res) => {
+    fs.readFile("./db/books.json", "utf-8", (err,data)=>{
+        if(err) {
+            console.log(err)
+            res.status(500).json({
+                msg:"oh no!",
+                err:err
+            })
+        } else {
+            const dataArr = JSON.parse(data)
+            console.log(req.params.id)
+            for (let i = 0; i < dataArr.length; i++) {
+                const book = dataArr[i];
+                if (book.id==req.params.id) {
+                    return res.json(book)
+                }
+            }
+            res.status(404).json({
+                msg:"book not found!"
+            })
+        }
+    })
+})
+app.post('/api/books/',(req, res) => {
+    fs.readFile("./db/books.json", "utf-8", (err,data)=>{
+        if(err) {
+            console.log(err)
+            res.status(500).json({
+                msg:"oh no!",
+                err:err
+            })
+        } else {
+            const dataArr = JSON.parse(data)
+            dataArr.push(req.body);
+            fs.writeFile("./db/books.json", JSON.stringify(dataArr),(err,data)=> {
+                if(err) {
+                    console.log(err)
+                    res.status(500).json({
+                        msg:"oh no!",
+                        err:err
+                    })  
+                }
+                else {
+                    res.json({
+                        msg:"succefully added!"
+                    })
+                }
+            })
+            
+        }
+    })
+})
+
+app.get('*',(req, res) => {
+    res.sendFile(path.join(__dirname,"./views/404.html"))
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,41 @@
 // add front end scripting here!
-alert("I AM LINKED")
+const booksUl = document.querySelector("#allBooks");
+const bookForm = document.querySelector("#newBook");
+const bookIdInput = document.querySelector("#bookId");
+const bookTitleInput = document.querySelector("#bookTitle");
+const bookAuthorInput = document.querySelector("#bookAuthor");
+
+fetch("/api/books").then(res=>{
+    return res.json()
+}).then(data=>{
+    console.log(data);
+    data.forEach(book => {
+        const bookLi = document.createElement("li");
+        bookLi.innerHTML=`<strong>${book.id}.</strong> <em>${book.title}</em> by ${book.author}`;
+        booksUl.append(bookLi);
+    });
+})
+
+
+bookForm.addEventListener("submit", e=>{
+    e.preventDefault();
+    const bookData = {
+        id: parseInt(bookIdInput.value),
+        title: bookTitleInput.value,
+        author: bookAuthorInput.value,
+    }
+    console.log(bookData);
+    fetch("/api/books", {
+        method:"POST",
+        body:JSON.stringify(bookData),
+        header:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok) {
+            location.reload()
+        } else {
+            alert("trumpet sound")
+        }      
+    })
+})
